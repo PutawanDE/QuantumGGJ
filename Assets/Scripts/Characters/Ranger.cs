a800d1b6fd62d8b8d89a54b1245dc0186bfa4971
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2023-02-04 15:29:34 +0700

    Add shooting arrow

--- Assets/Scripts/Characters/Ranger.cs
+++ Assets/Scripts/Characters/Ranger.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Ranger : Character
 {
+    [SerializeField] private GameObject arrow;
+    [SerializeField] private float arrowSpeed;
+
     private void Awake()
     {
         attackRange = 10.0f;
@@ -12,5 +15,12 @@ public class Ranger : Character
         hp = maxHp;
         walkSpeed = 15.0f;
         attackRatePerSec = 2.0f;
+    }
+
+    protected override void Attack()
+    {
+        Arrow spawnedArrow = Instantiate(arrow, transform.position, Quaternion.identity).GetComponent<Arrow>();
+        Vector2 direction = facingLeft ? Vector2.left : Vector2.right;
+        spawnedArrow.Initialize(gameObject.tag, attackDamage, arrowSpeed, direction);
     }
 }
