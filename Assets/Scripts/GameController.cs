a10781c57406aac6174e2f9906d5a7d2dd52624f
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2023-02-04 18:56:59 +0700

    Fix inconsisten ground check

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject[] characters;
     [SerializeField] private Vector2 playerSpawnPoint;
     [SerializeField] private Vector2 enemySpawnPoint;
-    
+    [SerializeField] private CinemachineVirtualCamera cam;
+
     private GameObject player;
     private GameObject enemy;
 
@@ -26,6 +29,7 @@ public class GameController : MonoBehaviour
     {
         player = Instantiate(player, playerSpawnPoint, Quaternion.identity);
         player.GetComponent<Character>().Initialize("Player");
+        cam.Follow = player.transform;
 
         enemy = Instantiate(enemy, enemySpawnPoint, Quaternion.identity);
         enemy.GetComponent<Character>().Initialize("Enemy");
